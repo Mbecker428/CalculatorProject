274780995461855034ac64e6d29b67ac084892fb
Marisabel Becker <mbecker@hireology.com>
2022-09-22 16:20:38 -0400

    Changed object names

--- NewAdvancedCanculator/Program.cs
+++ NewAdvancedCanculator/Program.cs
@@ -26,30 +26,30 @@ namespace NewAdvancedCalculator
                 Console.Write("Type 1 or 2 and press enter.\n");
                 Console.Write("");
 
-                SimpleCalculations calculate = new SimpleCalculations();
-                AdvancedCalculations calc = new AdvancedCalculations();
-                string menuOption = calculate.choiceValidation(Console.ReadLine());
+                SimpleCalculations simple = new SimpleCalculations();
+                AdvancedCalculations advance = new AdvancedCalculations();
+                string menuOption = simple.choiceValidation(Console.ReadLine());
                 
                 if (menuOption == "1")
                 {
                     Console.Write("Please enter the first number and press enter: ");
-                        double firstValue = calculate.inputValidation(Console.ReadLine());
+                        double firstValue = simple.inputValidation(Console.ReadLine());
                     
                     Console.Write("Please enter the second number and press enter: ");
-                        double secondValue = calculate.inputValidation(Console.ReadLine());
+                        double secondValue = simple.inputValidation(Console.ReadLine());
                         
                     Console.Write("Please enter the operator and press enter: ");
-                        string symbol = calculate.Operator(Console.ReadLine());
-                        calculate.simpleCalculation(firstValue, secondValue, symbol);
+                        string symbol = simple.Operator(Console.ReadLine());
+                        simple.simpleCalculation(firstValue, secondValue, symbol);
                 }
                 else if (menuOption == "2")
                 {
                     Console.Write("Please enter the bill amount and press enter: ");
-                        double billAmount = calculate.inputValidation(Console.ReadLine());
+                        double billAmount = simple.inputValidation(Console.ReadLine());
                     
                     Console.Write("Please enter the tip rate and press enter: ");
-                        string tipRate = calc.TipType(Console.ReadLine());
-                        calc.TipCalculation(billAmount, tipRate);
+                        string tipRate = advance.TipType(Console.ReadLine());
+                        advance.TipCalculation(billAmount, tipRate);
                 }
                 else
                 {
@@ -59,7 +59,7 @@ namespace NewAdvancedCalculator
                 }
                 
                 Console.Write("Do you want to try again? Y/y or N/n ");
-                string tryAgain = calculate.exitValidation(Console.ReadLine());
+                string tryAgain = simple.exitValidation(Console.ReadLine());
                 if (tryAgain == "Y" || tryAgain == "y") Console.Clear();
                     else if (tryAgain == "N" || tryAgain == "n")
                 {
